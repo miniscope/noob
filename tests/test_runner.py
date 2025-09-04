e1b5e7fa6b0257fffdd3d2faa45e2555d184749c
Raymond <raymond.w.jang@gmail.com>
2025-09-04 03:37:21 -0700

    test: event callback test

---
+++ tests/test_runner.py
@@ -0,0 +1,21 @@
+from noob import Tube, SynchronousRunner
+
+
+def test_process_callback() -> None:
+
+    tube = Tube.from_specification("testing-basic")
+    runner = SynchronousRunner(tube)
+
+    runner.init()
+    events = []
+
+    def _cb(event):
+        if event["node_id"] == "whatever":
+            return
+        nonlocal events
+        events.append(event)
+
+    runner.add_callback(_cb)
+    runner.process()
+
+    assert len(events) == 2
