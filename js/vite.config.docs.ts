7865c7f05e611a70078a9ba2144b1f7060da6cd2
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2025-12-13 00:14:24 -0800

    test readthedocs build

--- js/vite.config.docs.ts
+++ js/vite.config.docs.ts
@@ -1,29 +1,11 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'node:path'
-import react from '@vitejs/plugin-react'
+import { mergeConfig } from 'vite'
+
+import DocsConfig from './vite.config.dev.docs'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default mergeConfig(DocsConfig, {
   build: {
-    target: "baseline-widely-available",
-    lib: {
-      entry: resolve(__dirname, 'src/main_docs.ts'),
-      name: "noob_js",
-      fileName: 'js/noob-js',
-      cssFileName: "css/noob-js",
-    },
-    outDir: resolve(__dirname, '../docs/_static'),
-    emptyOutDir: false,
+    minify: true,
     sourcemap: true,
-  },
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  define: {
-    'process.env': {}
   }
 })
