76c3dfa624c39a67943752637334fcf3df005c88
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2025-12-12 19:13:03 -0800

    hello world from the js plotter

---
+++ js/src/main.tsx
@@ -0,0 +1,20 @@
+import { createRoot } from "react-dom/client";
+import { NoobFlow } from "./flow.tsx";
+import { ReactFlowProvider } from "@xyflow/react";
+
+import type { TubeSpecification } from "./types.ts";
+
+import "./index.css";
+
+export const renderPipeline = (selector: string, tube: TubeSpecification) => {
+  const node = document.querySelector(selector);
+  if (node === null) {
+    throw Error("selector not found");
+  }
+  const root = createRoot(node);
+  root.render(
+    <ReactFlowProvider>
+      <NoobFlow tube={tube}></NoobFlow>
+    </ReactFlowProvider>,
+  );
+};
---
+++ js/src/types.ts
@@ -0,0 +1,23 @@
+import { type Node } from "@xyflow/react";
+
+export interface TubeSpecification {
+  noob_id: string;
+  noob_mode: string;
+  noob_version: string;
+  nodes: Record<string, NoobNode>;
+}
+
+export interface NoobNode {
+  type: string;
+  depends: Record<string, string>[];
+}
+
+// Node class expects a type with string keys, not interface
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+export type ElkNodeData = {
+  label: string;
+  sourceHandles: { id: string }[];
+  targetHandles: { id: string }[];
+};
+
+export type ElkNode = Node<ElkNodeData, "elk">;
