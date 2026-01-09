a382e00e675a839537ffa5ae006209981212e52b
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-01-08 17:36:42 -0800

    update test fixtures after rename

--- tests/test_pipelines/test_input.py
+++ tests/test_pipelines/test_input.py
@@ -7,20 +7,20 @@ from noob.runner.base import TubeRunner
 pytestmark = pytest.mark.input
 
 
-def test_tube_input_params(runner_cls: type[TubeRunner]):
+def test_tube_input_params(sync_runner_cls: type[TubeRunner]):
     """tube-scoped input can be used as params"""
     tube = Tube.from_specification("testing-input-tube-params", input={"start": 7})
-    runner = runner_cls(tube)
+    runner = sync_runner_cls(tube)
     with runner:
         outputs = [runner.process() for _ in range(5)]
         assert len(outputs) == 5
         assert outputs == [7, 8, 9, 10, 11]
 
 
-def test_tube_input_depends(runner_cls: type[TubeRunner]):
+def test_tube_input_depends(sync_runner_cls: type[TubeRunner]):
     """tube-scoped input can be used as depends"""
     tube = Tube.from_specification("testing-input-tube-depends", input={"multiply_right": 7})
-    runner = runner_cls(tube)
+    runner = sync_runner_cls(tube)
     with runner:
         outputs = [runner.process() for _ in range(5)]
         assert len(outputs) == 5
@@ -72,7 +72,7 @@ def test_process_input_missing(loaded_tube, runner):
     assert runner.process(multiply_right=10) == 0
 
 
-def test_input_integration(runner_cls):
+def test_input_integration(sync_runner_cls):
     """
     All the different forms of input can be used together
     """
@@ -83,7 +83,7 @@ def test_input_integration(runner_cls):
         "testing-input-mixed", input={"start": start, "multiply_tube": multiply_tube}
     )
 
-    runner = runner_cls(tube)
+    runner = sync_runner_cls(tube)
     with runner:
         for i in range(5):
             this_process = multiply_process * 2 * (i + 1)
