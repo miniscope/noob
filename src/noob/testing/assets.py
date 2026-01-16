e17411ea5430664c183244b9e35e57eda123feb0
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-01-15 16:31:42 -0800

    actually not importing xarray

--- src/noob/testing/assets.py
+++ src/noob/testing/assets.py
@@ -1,15 +1,15 @@
 import sqlite3
 
 import numpy as np
-import xarray as xr
+# import xarray as xr
 
 
-def xarray_asset() -> xr.DataArray:
-    return xr.DataArray(
-        np.ones((3, 4, 5), dtype=float),
-        dims=["x", "y", "z"],
-        coords={"x": np.arange(0, 3, 1), "y": np.arange(0, 4, 1), "z": np.arange(0, 5, 1)},
-    )
+# def xarray_asset() -> xr.DataArray:
+#     return xr.DataArray(
+#         np.ones((3, 4, 5), dtype=float),
+#         dims=["x", "y", "z"],
+#         coords={"x": np.arange(0, 3, 1), "y": np.arange(0, 4, 1), "z": np.arange(0, 5, 1)},
+#     )
 
 
 def db_connection() -> sqlite3.Connection:
