ce0e36040d9bb95026037186078b47cdb87e5baf
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-01-16 14:10:10 -0800

    temporary cli

---
+++ src/noob/cli/run.py
@@ -0,0 +1,47 @@
+import click
+import json
+import sys
+from noob.runner import SynchronousRunner, AsyncRunner, TubeRunner
+from noob.runner.zmq import ZMQRunner
+from noob import Tube
+from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn, TextColumn
+from rich.console import Console
+_runners: dict[str, type[TubeRunner]] = {
+    "sync": SynchronousRunner,
+    "async": AsyncRunner,
+    "zmq": ZMQRunner,
+}
+
+@click.command("run")
+@click.option("--tube", required=True)
+@click.option("--runner", type=click.Choice(("sync", "async", "zmq")), default="sync")
+@click.option("--n", "-n", type=click.INT)
+@click.option("--format", type=click.Choice(("json",)))
+def run(tube: str, runner: str = "sync", n: int | None = None, format: str | None = None) -> None:
+    tube_id = tube
+    tube = Tube.from_specification(tube)
+    runner_cls = _runners[runner]
+    runner=  runner_cls(tube)
+    if n is None:
+        raise NotImplementedError("Just a demo!")
+    else:
+
+        console = Console(file=sys.stderr)
+        progress = Progress(
+            TextColumn(f"[bold green]{tube_id}[/bold green]"),
+            SpinnerColumn(),
+            *Progress.get_default_columns(),
+            TimeElapsedColumn(),
+            transient=True,
+            console=console
+        )
+    results = []
+    with runner, progress:
+        task = progress.add_task("Running", total=n)
+        for result in runner.iter(n=n):
+            results.append(result)
+            progress.advance(task)
+
+
+    if format == "json":
+        click.echo(json.dumps(results))
--- src/noob/testing/nodes.py
+++ src/noob/testing/nodes.py
@@ -168,7 +168,7 @@ def read_db(conn: sqlite3.Connection) -> A[tuple[int, str], Name("payload")]:
 
 
 def long_add(value: float) -> float:
-    sleep(0.25)
+    sleep(0.01)
     return value + 1
 
 
