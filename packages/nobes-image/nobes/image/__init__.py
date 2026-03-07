44019f8451082b6535c37ff18c1e12fde6ae6e97
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-03-06 21:03:06 -0800

    nobes image, nobes os, and file watching/ocr example

---
+++ packages/nobes-image/nobes/image/__init__.py
@@ -0,0 +1,9 @@
+from nobes.image.convert import convert
+from nobes.image.io import read_image
+from nobes.image.ocr import tesseract
+
+__all__ = [
+    "convert",
+    "read_image",
+    "tesseract",
+]
