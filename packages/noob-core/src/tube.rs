662e15529a99485fe969f6e6e93ea2992e23d39d
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-07-16 18:56:09 -0700

    linting, actually install dev deps in docs test

--- packages/noob-core/src/tube.rs
+++
@@ -1,9 +0,0 @@
-//! Tube helpers from rust :)
-//! Just mirroring the python structure for now for ported code
-
-use crate::FxIndexSet;
-use crate::sorter::EdgeRec;
-
-#[cfg(test)]
-#[path = "tests/tube.rs"]
-mod tests;
